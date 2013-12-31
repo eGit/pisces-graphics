e74ffed581381bed9bbf5b30b9cc925687d1bd42
jdp <jdp@ulsf.net>
2013-12-31 19:08:34 +0000

    classpath exception update

--- src/pisces/d/PathSink.java
+++ src/pisces/d/PathSink.java
@@ -2,9 +2,13 @@
  * Copyright (C) 2010 John Pritchard
  * Copyright  1990-2008 Sun Microsystems, Inc. All Rights Reserved. 
  *  
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License version 
- * 2 only, as published by the Free Software Foundation. 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.  The copyright
+ * holders designate particular file as subject to the "Classpath"
+ * exception as provided in the LICENSE file that accompanied this
+ * code.
  *  
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
